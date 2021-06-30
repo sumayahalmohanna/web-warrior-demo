78b56e6b55b4be0eb216d7de26158248ce68afcd
sumayahalmohanna <sumayahsaur@gmail.com>
2021-06-30 06:08:37 -0400

    testing

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { GatsbyImage } from 'gatsby-plugin-image';
 import * as styles from '../styles/Project-details.module.css';
 import { graphql } from 'gatsby';
 
@@ -10,11 +10,11 @@ const ProjectDetails = ({ data }) => {
 
     return ( 
         <Layout>
-            <div className={styles.detials}>
+            <div className={styles.details}>
                 <h2>{title}</h2>
                 <h3>{stack}</h3>
                 <div className={styles.featured}>
-                    <GatsbyImage image={featuredImg.childImageSharp.gatsbyImageData} alt='this is an alt'/>
+                    <GatsbyImage image={featuredImg.childImageSharp.gatsbyImageData } alt='this is an alt'/>
                 </div>
                 {/**dangerous b/c of server side cross scripting attack */}
                 <div className={styles.html} dangerouslySetInnerHTML={{__html: html}} />
